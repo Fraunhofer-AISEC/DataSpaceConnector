b337dd9e607ec7695b5c26451ca6de90d0d919cb
Jean-Luc Reding <jean-luc.reding@aisec.fraunhofer.de>
2023-03-22 15:05:30 +0100

    Added test classes for idscp2

---
+++ extensions/data-plane/data-plane-idscp2/src/test/java/org/eclipse/edc/connector/dataplane/idscp2/DataPlaneIdscp2ExtensionTest.java
@@ -0,0 +1,30 @@
+package org.eclipse.edc.connector.dataplane.idscp2;
+
+import org.eclipse.edc.junit.extensions.EdcExtension;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+@ExtendWith(EdcExtension.class)
+public class DataPlaneIdscp2ExtensionTest {
+    @BeforeAll
+    public static void setUp() {
+       //
+    }
+
+    @AfterAll
+    public static void tearDown() {
+        //
+    }
+
+    @Test
+    void sendToClient(){
+        //
+    }
+
+    @Test
+    void receive(){
+        //
+    }
+}
---
+++ extensions/data-plane/data-plane-idscp2/src/test/java/org/eclipse/edc/connector/dataplane/idscp2/pipeline/Idscp2ServerTest.java
@@ -0,0 +1,10 @@
+package org.eclipse.edc.connector.dataplane.idscp2.pipeline;
+
+import org.junit.jupiter.api.Test;
+
+public class Idscp2ServerTest {
+    @Test
+    void testServer(){
+        //
+    }
+}
